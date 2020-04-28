cadca7dd2b8477db44fdafd1db31e34d6a7993fb
Purvesh <ind.purvesh@gmail.com>
2020-04-28 21:01:46 +1000

    few updates on property option component

--- resources/components/core/catalog/AvoRedPropertyOption.vue
+++ resources/components/core/catalog/AvoRedPropertyOption.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
         <div 
-            class="mt-1 sm:mt-0 sm:col-span-2 relative" 
-            v-for="option in options"
+            class="mt-3 sm:col-span-2 relative" 
+            v-for="(option, i) in options"
             :key="'property-option-' + option.key"
         >
             <div class="max-w-lg flex rounded-md shadow-sm">
                 <input
+                    :name="'options['+ option.key +']'"
                     class="form-input flex-1 block w-full px-3 py-2 
                         focus:border-gray-200 focus:shadow-none rounded-none rounded-l-md 
                         sm:text-sm sm:leading-5 border-r-0"
                 />
-                <button class="inline-flex items-center cursor-pointer px-3 rounded-r-md border border-gray-300 bg-gray-50 text-gray-900 sm:text-sm">
-                    + Add
+                <button 
+                    type="button"
+                    @click="addRow()"
+                    class="inline-flex items-center cursor-pointer px-3 rounded-r-md border border-gray-300 bg-gray-50 text-gray-900 sm:text-sm">
+                    {{ buttonLabel(i) }}
                 </button>
             </div>
         </div>
@@ -25,8 +29,26 @@ export default {
     props: {
 
     },
-    methods() {
-        
+    methods: {
+        randomString(stringLength = 6) {
+            let str = 'abcdefghijklmnopqrstuvwxyz'
+            return str.split('').sort(() =>  0.5-Math.random() ).join('').substring(0, stringLength);
+        },
+        addRow() {
+            this.options.push({
+                key: this.randomString()
+            });
+        },
+        buttonLabel(i) {
+            if (i === 0 && (this.options.length - 1) === 0) {
+                return 'Add'
+            }
+
+            if ((i === (this.options.length - 1))) {
+                return 'Add'
+            }
+            return 'Remove'
+        }
     },
     data() {
         return {
@@ -34,8 +56,10 @@ export default {
         }
     },
     mounted() {
+        let optionKey = this.randomString()
+
         this.options.push({
-            key : 'option1'
+            key : optionKey
         })
     }
 }
