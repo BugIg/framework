9dd95a85e3a29bf48099db9b36902d2651b4dac9
Purvesh <ind.purvesh@gmail.com>
2020-05-03 16:11:02 +1000

    added app key for testing

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -27,8 +27,7 @@ abstract class BaseTestCase extends OrchestraTestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->app['config']->set('app.key', 'base64:UTyp33UhGolgzCK5CJmT+hNHcA+dJyp3+oINtX+VoPI=');
-
+        
         $this->tablePrefix = $this->app['config']->get('avored.table_prefix');
 
         $this->withFactories(__DIR__.('/../database/factories'));
