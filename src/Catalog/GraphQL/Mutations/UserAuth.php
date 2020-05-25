6ded44768ac1b4df8d48d9c1cb7425f8659ac633
Purvesh <ind.purvesh@gmail.com>
2020-05-25 20:03:58 +1000

    auth trait added

--- src/Catalog/GraphQL/Mutations/UserAuth.php
+++ src/Catalog/GraphQL/Mutations/UserAuth.php
@@ -2,16 +2,16 @@
 
 namespace AvoRed\Framework\Catalog\GraphQL\Mutations;
 
+use AvoRed\Framework\Catalog\GraphQL\Traits\AuthTrait;
 use AvoRed\Framework\User\Models\User;
 use GraphQL\Type\Definition\ResolveInfo;
-use Laminas\Diactoros\ServerRequest;
-use Laravel\Passport\Bridge\Client as BridgeClient;
 use Laravel\Passport\Client;
-use Laravel\Passport\Http\Controllers\AccessTokenController;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class UserAuth
 {
+    use AuthTrait;
+    
     /**
      * Return a value for the field.
      *
@@ -22,52 +22,18 @@ class UserAuth
      * @return mixed
      */
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
-    {
-        $data = [];
+    {   
         $user = $this->getUser($args);
-        $client = $user->getPassportClient();
-        
+        $client = $user->getPassportClient();  
     
         if (null !== $client && $client instanceof Client) {
-           
-            $serverRequest = $this->createRequest($client, $user->id, $args, $scope = []);
-            $reponse = app(AccessTokenController::class)->issueToken($serverRequest);
-            $data = json_decode($reponse->content(), true);
-            
-            $user->token_type = $data['token_type'];
-            $user->expires_in = $data['expires_in'];
-            $user->access_token = $data['access_token'];
-            $user->refresh_token = $data['refresh_token'];
-           
-           
+            $user = $this->validateUser($client, $user, $args);   
             return $user;
         }
-
-       
         
         return null;
     }
 
-     /**
-     * Create a request instance for the given client.
-     *
-     * @param  \Laravel\Passport\Client  $client
-     * @param  mixed  $userId
-     * @param  array  $scopes
-     * @return \Zend\Diactoros\ServerRequest
-     */
-    protected function createRequest($client, $userId, $data, array $scopes)
-    {
-        return (new ServerRequest)->withParsedBody([
-            'grant_type' => 'password',
-            'client_id' => $client->id,
-            'client_secret' => $client->secret,
-            'username' => $data['email'],
-            'password' => $data['password'],
-            'user_id' => $userId,
-            'scope' => implode(' ', $scopes),
-        ]);
-    }
     /**
      *
      * @param array $data
